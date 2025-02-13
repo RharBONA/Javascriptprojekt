13fbc08836e5e3317a416969d3d9fd1a8f8fec59
RharBONA <rharbonaofficial@hotmail.com>
2025-02-13 15:51:43 +0100

    billeder loader nu.

--- JSprojekt/jscript/ilyas.js
+++ JSprojekt/jscript/ilyas.js
@@ -19,14 +19,14 @@ function checkCarAvailability() {
 
 // Arrays (Liste over Audi-modeller)
 const models = [
-    { name: "Audi R8", img1: "../images/audi_r8.png", img2: "../images/audi_r8_alt.png", type: "Racerbil" },
-    { name: "Audi e-tron", img1: "../images/audi_etron.png", img2: "../images/audi_etron_alt.png", type: "Elbil" },
-    { name: "Audi Q7", img1: "../images/audi_q7.png", img2: "../images/audi_q7_alt.png", type: "Familiebil" },
-    { name: "Audi A6 Allroad", img1: "../images/audi_a6_allroad.png", img2: "../images/audi_a6_allroad_alt.png", type: "Arbejdsbil" },
-    { name: "Audi Q8", img1: "../images/audi_q8.png", img2: "../images/audi_q8_alt.png", type: "Stor bil" },
-    { name: "Audi RS6 Avant", img1: "../images/audi_rs6.png", img2: "../images/audi_rs6_alt.png", type: "Limited edition" },
-    { name: "Audi A1", img1: "../images/audi_a1.png", img2: "../images/audi_a1_alt.png", type: "Lille bil" },
-    { name: "Audi A3 TDI", img1: "../images/audi_a3_tdi.png", img2: "../images/audi_a3_tdi_alt.png", type: "Økonomisk bil" }
+    { name: "Audi R8", img1: "/JSprojekt/Billeder/audi_r8.png", img2: "/JSprojekt/Billeder/audi_r8_alt.png", type: "Racerbil" },
+    { name: "Audi e-tron", img1: "/JSprojekt/Billeder/audi_etron.png", img2: "/JSprojekt/Billeder/audi_etron_alt.png", type: "Elbil" },
+    { name: "Audi Q7", img1: "/JSprojekt/Billeder/audi_q7.png", img2: "/JSprojekt/Billeder/audi_q7_alt.png", type: "Familiebil" },
+    { name: "Audi A6 Allroad", img1: "/JSprojekt/Billeder/audi_a6_allroad.png", img2: "/JSprojekt/Billeder/audi_a6_allroad_alt.png", type: "Arbejdsbil" },
+    { name: "Audi Q8", img1: "/JSprojekt/Billeder/audi_q8.png", img2: "/JSprojekt/Billeder/audi_q8_alt.png", type: "Stor bil" },
+    { name: "Audi RS6 Avant", img1: "/JSprojekt/Billeder/audi_rs6.png", img2: "/JSprojekt/Billeder/audi_rs6_alt.png", type: "Limited edition" },
+    { name: "Audi A1", img1: "/JSprojekt/Billeder/audi_a1.png", img2: "/JSprojekt/Billeder/audi_a1_alt.png", type: "Lille bil" },
+    { name: "Audi A3 TDI", img1: "/JSprojekt/Billeder/audi_a3_tdi.png", img2: "/JSprojekt/Billeder/audi_a3_tdi_alt.png", type: "Økonomisk bil" }
 ];
 
 // DOM (Manipulation af HTML-struktur)
